29fd80d6eca5c083c991f2077c6d32b01054b161
fabia arbab <f2025-0249@bnu.edu.pk>
2025-10-24 03:02:54 -0700

    diamond.cpp

---
+++ inverse pyramid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of diamond: ";
+    cin >> n;
+
+    // Upper pyramid
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++)
+            cout << " ";
+        for (int k = 1; k <= (2 * i - 1); k++)
+            cout << "*";
+        cout << endl;
+    }
+
+    // Lower pyramid
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--)
+            cout << " ";
+        for (int k = 1; k <= (2 * i - 1); k++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
